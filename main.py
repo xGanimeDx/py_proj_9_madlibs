b262b76e7ba8278ed7c75fc4a3cdd32ac740140e
sradevich <stanislav.radevich@zenitech.co.uk>
2022-06-13 13:59:01 +0300

    add madlibs code

---
+++ main.py
@@ -0,0 +1,13 @@
+noun = input("Enter a noun: ")
+plural_noun_1 = input("Enter a plural noun: ")
+verb_1 = input("Enter a verb: ")
+verb_2 = input("Enter a verb: ")
+part_of_body = input("Enter a part of body: ")
+adjective_1 = input("Enter an adjective: ")
+plural_noun_2 = input("Enter a plural noun: ")
+adjective_2 = input("Enter an adjective: ")
+print(f"Today, every student has a computer small enough to fit into his {noun}. He can solve \
+any math problem by simply pushind the computer's little {plural_noun_1}. Computers \
+can add, multiply, divide, and {verb_1}. They can also {verb_2} better than a human. \
+Some computers are {part_of_body}. Others have a/an {adjective_1} screen \
+that shows all kinds of {plural_noun_2} and {adjective_2} figures.")
